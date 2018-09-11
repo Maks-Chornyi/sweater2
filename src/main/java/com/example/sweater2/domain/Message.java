20d0c0ddd3ca3b8aea32048d2c0e495cbf94d48c
Maks-Chornyi <380960310411@mail.ua>
2018-09-11 13:37:49 +0300

    dev: dropped and created new db, added migration files, added profile updating on profile page

--- src/main/java/com/example/sweater2/domain/Message.java
+++ src/main/java/com/example/sweater2/domain/Message.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     private String text;
 
